f8358d7a1c8ec13b55ab3297890796fd22d00322
Vincent Massias <vi.massias@gmail.com>
2023-05-23 11:03:09 +0200

    disable observers

--- battlescribe/bs_main.ts
+++ battlescribe/bs_main.ts
@@ -155,10 +155,10 @@ export class Base implements BSModifierBase {
     }
   }
   // Prevent Vue Observers
-  /*   get [Symbol.toStringTag](): string {
+  get [Symbol.toStringTag](): string {
     // Anything can go here really as long as it's not 'Object'
     return "ObjectNoObserve";
-  } */
+  }
   isGroup(): boolean {
     return false;
   }
@@ -369,7 +369,7 @@ export class Base implements BSModifierBase {
         if (isObject(value)) {
           if (Array.isArray(value)) {
             if (value.length && isObject(value[0])) {
-              for (let i = value.length; i--; ) {
+              for (let i = value.length; i--;) {
                 const cur = value[i];
                 callbackfn(cur, current);
                 stack.push(cur);
@@ -398,7 +398,7 @@ export class Base implements BSModifierBase {
         if (isObject(value)) {
           if (Array.isArray(value)) {
             if (value.length && isObject(value[0])) {
-              for (let i = value.length; i--; ) {
+              for (let i = value.length; i--;) {
                 const cur = value[i];
                 callbackfn(cur, current);
                 stack.push(cur);
