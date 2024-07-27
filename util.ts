8ee088745e0d0577f0a1783f884cdde1524ba6eb
Vincent Massias <vi.massias@gmail.com>
2024-07-27 17:20:24 +0200

    canceld change

--- util.ts
+++ util.ts
@@ -100,9 +100,9 @@ export function dateFormat(date: Date | string): string {
     date = new Date(date);
   }
 
-  const year = date.getUTCFullYear().toString().padStart(4, "0");
-  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
-  const day = date.getUTCDate().toString().padStart(2, "0");
+  const year = date.getFullYear().toString().padStart(4, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
 
   return `${year}-${month}-${day}`;
 }
